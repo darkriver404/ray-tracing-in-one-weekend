cc5c35692bb1c2130f06ecbf2421b1d0dc3ef86c
DarkRiver.Veda <darkriver404@outlook.com>
2018-02-07 20:49:01 +0800

    chapter2

---
+++ chapter2/chapter2.cpp
@@ -0,0 +1,25 @@
+// chapter2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <fstream>
+#include "vec3.h"
+
+int main()
+{
+	int nx = 200;
+	int ny = 100;
+
+	std::ofstream out("out.ppm");
+
+	out << "P3\n" << nx << " " << ny << "\n255\n";
+	for (int j = ny - 1; j >= 0; j--)
+	{
+		for (int i = 0; i < nx; i++)
+		{
+			vec3 color = vec3( float(i) / float(nx), float(j) / float(ny),0.2) * 255.99;
+			out << (int)color.r() << " " << (int)color.g() << " " << (int)color.b() << "\n";
+		}
+	}
+	return 0;
+}
